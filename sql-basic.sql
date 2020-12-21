816f4d92febfd22687f8ac0f7141af50a3ebe68c
OfWolfAndMan <therootbridge@hotmail.com>
2020-12-21 11:17:51 -0500

    Added Jupyter Notebook folder

---
+++ sql-basic.sql
@@ -0,0 +1,14 @@
+create table devices (
+    hostname VARCHAR(30) NOT NULL PRIMARY KEY,
+    management_ip inet NOT NULL,
+    model VARCHAR(30) NOT NULL,
+    vendor VARCHAR(20) NOT NULL CHECK (vendor in ('Cisco',
+    'Juniper', 'Arista', 'Fortinet', 'Checkpoint', 'HP', 'Brocade')),
+    credential_id VARCHAR(20) REFERENCES credentials (name)
+);
+
+create table credentials (
+    name VARCHAR(20) NOT NULL PRIMARY KEY,
+    username VARCHAR(20) NOT NULL,
+    password VARCHAR(25) NOT NULL
+);
